b987920f4153835c92bb7dd488a62815057b955f
Malaiu Octavian <malaiuoctavian@gmail.com>
2017-02-14 15:52:23 +0200

    Exercitiul 1

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double mile, km;
+    cout<<"Introduceti distanta in mile: ";
+    cin>>mile;
+    km=mile*1.609344;
+    cout<<"Distanta in km este de: "<<km;
+    return 0;
+}
